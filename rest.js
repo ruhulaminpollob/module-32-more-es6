8a49c592b743bb832aed6cf456035f25e39c4a69
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 21:42:20 +0600

    rest object and rest array added

--- rest.js
+++ rest.js
@@ -4,3 +4,16 @@ const array=[2,3,5,65,7,3,9];
 const [a,b,c,...p]=array;
 console.log(p);
 console.log(b);
+// rest in object
+const object={
+    name:'pollob',
+    age:19,
+    work:'web dev',
+    hight:5.8,
+    wight:65,
+
+}
+
+const {name,work,...r}=object;
+console.log(r);
+console.log(name,work);
