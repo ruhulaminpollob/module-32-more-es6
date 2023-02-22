a9e15c4b914d57e8f51382182c0a9108e0e7b547
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 23:51:35 +0600

    es6 for each explored

---
+++ for-each.js
@@ -0,0 +1,7 @@
+const phones=[
+    {id:1, item:'mobile', name:'samsung', price:70000},
+    {id:1, item:'laptop', name:'asus', price:180000},
+    {id:1, item:'watch', name:'samsung', price:30000},
+    {id:1, item:'cycle', name:'phoenix', price:15000},
+]
+const each=phones.forEach(x =>console.log(x.name,'tore khisi'));
