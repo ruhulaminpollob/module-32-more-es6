5fe452a5e35065a3988f87429c4571530125d846
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 22:37:44 +0600

    make array element double using functions

--- map.js
+++ map.js
@@ -1,8 +1,20 @@
-const numbers=[2,4,5,6,7,9];
+const numbers = [2, 4, 5, 6, 7, 9];
 
 // make numbers double using for loop
-const double1=[];
-for (const number of numbers) {   
-    double1.push(number*2);
+const double1 = [];
+for (const number of numbers) {
+    double1.push(number * 2);
 }
-console.log(double1);
+// console.log(double1);
+
+// make numbers of array double using functions
+
+function double2(num) {
+    const double1 = [];
+    for (const number of numbers) {
+        double1.push(double3(number));
+    }
+    return double1;
+}
+const double3=num=>num*2;
+console.log(double2(numbers));
