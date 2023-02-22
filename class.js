f0df72d6d28f6a5b928b9e05fd18714e5a8fe6d6
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-23 01:30:04 +0600

    more es6 class practice

--- class.js
+++ class.js
@@ -26,3 +26,26 @@ const pollob = new student('Pollob', 5);
 console.log(pollob);
 pollob.assignment('3rd March');
 pollob.studyTime(4);
+
+// class more practice
+
+class teacher{
+    name;
+    work='teaching';
+    classTime;
+    honest=true;
+    constructor(name, classTime){
+        this.name=name;
+        this.classTime=classTime;
+    }
+
+    schedule(time1,time2){
+        console.log(
+            `${time1} to ${time2}`
+        );
+    }
+}
+
+const kashemSir=new teacher('kashemSir',10.30);
+console.log(kashemSir);
+kashemSir.schedule(9,2)
